4559746838de8b5fc7ee4de6d5957597d02f4e0b
LeonelMaldonado <leonelmaldonado18@outlook.com>
2024-06-15 23:52:20 -0500

    Some Updates Of My Thomas
    Some Updates In The Scenes And UI

---
+++ Assets/Scrips/MenuPause.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour
+{
+public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+}
